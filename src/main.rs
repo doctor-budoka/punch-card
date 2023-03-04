9a83fe29e414aa3abc76336a6b8485b56a0f889b
James Boland <jamespatrickboland@gmail.com>
2023-03-04 21:11:52 +0000

    The default config file is now written if it doesn't exist

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use std::fs::{File, OpenOptions, create_dir_all,read_to_string};
 use std::io::Write;
 use std::path::Path;
-use chrono::prelude::{DateTime, Utc, Datelike, Month};
+use chrono::prelude::{DateTime, Utc};
 use std::env::{var, args};
 
 const DEFAULT_TIME_MINS: u32 = 480;
 const BASE_DIR: &str = "~/.punch-card/";
-const CONFIG_PATH: &str = "punch.cfg";
+const CONFIG_FILE: &str = "punch.cfg";
 
 enum SubCommand {
     In,
@@ -32,6 +32,18 @@ fn main() {
     let command_name: &String = &env_args[1];
     let command: SubCommand = SubCommand::from_string(command_name);
 
+    let base_dir_expanded: String = expand_path(BASE_DIR);
+    if !Path::new(&base_dir_expanded).exists() {
+        create_dir_all(base_dir_expanded).expect("Should be able to create directory!");
+    }
+    let config_path: String = expand_path(&(BASE_DIR.to_owned() + &(CONFIG_FILE.to_owned())));
+    if !Path::new(&config_path).exists() {
+        let default_content: String = format!("DAY_IN_MINUTES={DEFAULT_TIME_MINS}\nTIME_BEHIND=0");
+        // let mut cfg_file = OpenOptions::new().create_new(true).write(true).open(config_path).expect("Couldn't create a new config file");
+        // cfg_file.write_all(default_content.as_bytes()).expect("Couldn't write to config file!");
+        write_file(&config_path, default_content);
+    }
+
     match command {
         SubCommand::In => punch_in(),
         SubCommand::Out => punch_out(),
@@ -44,8 +56,8 @@ fn main() {
 
 fn punch_in() {
     let now: DateTime<Utc> = Utc::now();
-
-    println!("Clocking in for the day");
+    let now_string: String = now.format("%Y-%m-%d %H:%M:%S").to_string();
+    println!("Clocking in for the day at '{now_string}'");
 }
 
 fn punch_out() {
@@ -61,7 +73,8 @@ fn resume() {
 }
 
 fn write_file(path: &str, contents: String) {
-    let file_result: Result<File, std::io::Error> = OpenOptions::new().create_new(true).write(true).open(path);
+    let path_to_write: String = expand_path(path);
+    let file_result: Result<File, std::io::Error> = OpenOptions::new().create_new(true).write(true).open(path_to_write);
     if let Ok(mut file) = file_result {
         file.write_all(contents.as_bytes()).expect("Couldn't write to file!");
     }
