38bcc7b6c49a65a62d66d7b6313a8dc391556566
James Boland <jamespatrickboland@gmail.com>
2023-03-06 10:09:10 +0000

    Adding functions to see start and end of day and intervals

--- src/day.rs
+++ src/day.rs
@@ -8,49 +8,60 @@ pub struct Interval {
 }
 
 impl Interval {
-    fn new(start: &DtUtc) -> Self {
+    pub fn new(start: &DtUtc) -> Self {
         return Self {start: *start, end: None};
     }
 
-    fn new_now() -> Self {
+    #[allow(dead_code)]
+    pub fn new_now() -> Self {
         let now: DtUtc = Utc::now();
         return Self::new(&now);
     }
 
-    fn end_at(&mut self, end: &DtUtc) {
+    pub fn end_at(&mut self, end: &DtUtc) {
         self.end = Some(*end);
     }
 
-    fn end_now(&mut self) {
+    #[allow(dead_code)]
+    pub fn end_now(&mut self) {
         let now: DtUtc = Utc::now();
         self.end_at(&now);
     }
 
-    fn has_end(&self) -> bool {
+    pub fn has_end(&self) -> bool {
         return match self.end {
             Some(_) => true,
             None => false,
         };
     }
+
+    pub fn get_start(&self) -> DtUtc {
+        return self.start;
+    }
+
+    pub fn get_end(&self) -> Option<DtUtc> {
+        return self.end;
+    }
 }
 
 pub struct Day {
-    overall_interval: Interval,
-    breaks: Vec<Interval>,
-    on_break: bool,
+    pub overall_interval: Interval,
+    pub breaks: Vec<Interval>,
+    pub on_break: bool,
 }
 
 impl Day {
-    fn new(start: &DtUtc) -> Self {
+    pub fn new(start: &DtUtc) -> Self {
         return Self {overall_interval: Interval::new(start), breaks: Vec::new(), on_break: false};
     }
 
-    fn new_now() -> Self {
+    #[allow(dead_code)]
+    pub fn new_now() -> Self {
         let now: DtUtc = Utc::now();
         return Self::new(&now);
     }
 
-    fn end_day_at(&mut self, at: &DtUtc) -> Result<(), &str> {
+    pub fn end_day_at(&mut self, at: &DtUtc) -> Result<(), &str> {
         if self.overall_interval.has_end() {
             return Err("Can't end the day because the day has already ended!");
         }
@@ -61,12 +72,13 @@ impl Day {
         }
     }
 
-    fn end_day_now(&mut self) -> Result<(), &str> {
+    #[allow(dead_code)]
+    pub fn end_day_now(&mut self) -> Result<(), &str> {
         let now: DtUtc = Utc::now();
         return self.end_day_at(&now);
     }
 
-    fn start_break(&mut self, at: &DtUtc) -> Result<(), &str> {
+    pub fn start_break(&mut self, at: &DtUtc) -> Result<(), &str> {
         if self.on_break {
             return Err("Can't start a break because day is already on break");
         }
@@ -77,12 +89,13 @@ impl Day {
         }
     }
 
-    fn start_break_now(&mut self) -> Result<(), &str> {
+    #[allow(dead_code)]
+    pub fn start_break_now(&mut self) -> Result<(), &str> {
         let now: DtUtc = Utc::now();
         return self.start_break(&now);
     }
 
-    fn end_current_break_at(&mut self, at: &DtUtc) -> Result<(), &str> {
+    pub fn end_current_break_at(&mut self, at: &DtUtc) -> Result<(), &str> {
         if !self.on_break {
             return Err("Can't end the break: currently not on break!");
         }
@@ -93,8 +106,17 @@ impl Day {
         }
     }
 
-    fn end_current_break_now(&mut self) -> Result<(), &str> {
+    #[allow(dead_code)]
+    pub fn end_current_break_now(&mut self) -> Result<(), &str> {
         let now: DtUtc = Utc::now();
         return self.end_current_break_at(&now);
+    }
+
+    pub fn get_day_start(&self) -> DtUtc {
+        return self.overall_interval.get_start();
+    }
+
+    pub fn get_day_end(&self) -> Option<DtUtc> {
+        return self.overall_interval.get_end();
     }
 }
