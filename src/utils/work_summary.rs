62bb4d48f594646b84a1571606a9829f56321757
James Boland <jamespatrickboland@gmail.com>
2023-05-05 18:28:23 +0200

    Adding a WorkSummary struct

---
+++ src/utils/work_summary.rs
@@ -0,0 +1,20 @@
+use serde::{Serialize, Deserialize};
+
+#[derive(Debug,Serialize,Deserialize)]
+pub struct WorkSummary {
+    category: String,
+    project: String,
+    task: String,
+    summary: String,
+}
+
+impl WorkSummary {
+    pub fn new(category: String, project: String, task: String, summary: String) -> Self {
+        return WorkSummary {
+            category: category,
+            project: project,
+            task: task,
+            summary: summary,
+        };
+    }
+}
