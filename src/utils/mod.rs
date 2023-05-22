425157d7c35a366b7f8afcaaaa6d594f70a7c804
James Boland <jamespatrickboland@gmail.com>
2023-05-22 13:31:33 +0200

    Keeping file_io inside it's own mod

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,5 +1,5 @@
 mod file_io;
-pub use file_io::{expand_path, read_file, write_file, create_dir_if_not_exists, create_base_dir_if_not_exists, BASE_DIR};
+pub use file_io::file_io::{expand_path, read_file, write_file, create_dir_if_not_exists, create_base_dir_if_not_exists, BASE_DIR};
 mod config;
 pub use config::{Config, create_default_config_if_not_exists, get_config_path, get_config, update_config};
 mod day;
