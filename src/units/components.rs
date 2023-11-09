09f5def053a59a964e1c0359f632d5715c0abfc6
James Boland <jamespatrickboland@gmail.com>
2023-11-09 07:17:35 +0100

    Task -> TimeBlock and methods added

--- src/units/components.rs
+++ src/units/components.rs
@@ -1,7 +1,7 @@
 use chrono::prelude::{DateTime, Local};
 use serde::{Serialize, Deserialize};
 
-use crate::units::interval::{Dt,Interval, DATE_FMT, DATETIME_FMT};
+use crate::units::interval::{Dt,Interval};
 
 #[derive(Debug,Serialize,Deserialize)]
 pub struct Note {
@@ -19,8 +19,66 @@ impl Note {
 }
 
 #[derive(Debug,Serialize,Deserialize)]
-pub struct Task {
-    pub name: String,
-    pub interval: Interval,
-    pub desc: Option<String>,
+pub struct TimeBlock {
+    task_name: String,
+    interval: Interval,
+    notes: Vec<Note>,
+}
+
+impl TimeBlock {
+    pub fn new(task_name: String, start: &DateTime<Local>) -> Self {
+        return Self {
+            task_name: task_name,
+            interval: Interval::new(start),
+            notes: Vec::new(),
+        };
+    }
+
+    pub fn get_task_name(&self) -> String {
+        return self.task_name.clone();
+    }
+
+    pub fn end_at(&mut self, end: &DateTime<Local>) {
+        self.interval.end_at(end);
+    }
+
+    pub fn has_end(&self) -> bool {
+        return self.interval.has_end();
+    }
+
+    pub fn get_start(&self) -> Dt {
+        return self.interval.get_start();
+    }
+
+    #[allow(dead_code)]
+    pub fn get_start_as_str(&self) -> String {
+        return self.interval.get_start_as_str();
+    }
+
+    pub fn get_end(&self) -> Option<Dt> {
+        return self.interval.get_end();
+    }
+
+    pub fn get_end_as_str(&self) -> Option<String> {
+        return self.interval.get_end_as_str();
+    }
+
+    #[allow(dead_code)]
+    pub fn as_string(&self) -> String {
+        return serde_yaml::to_string(&self).unwrap();
+    }
+
+    #[allow(dead_code)]
+    pub fn from_string(yaml_str: &String) -> Self {
+        return serde_yaml::from_str(yaml_str).unwrap();
+    }
+
+    pub fn get_length(&self) -> Option<i64> {
+        return self.interval.get_length();
+    }
+
+    pub fn add_note(&mut self, time: &DateTime<Local>, msg: &String) {
+        let new_note: Note = Note::new(time, msg);
+        self.notes.push(new_note);
+    }
 }
