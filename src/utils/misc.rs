2e339383ad9bdc5cbdc5b182661085fca41d21ae
James Boland <jamespatrickboland@gmail.com>
2025-02-19 09:21:15 +0100

    Using a more friendly but complicated time rendering fn

--- src/utils/misc.rs
+++ src/utils/misc.rs
@@ -1,3 +1,16 @@
 pub fn render_seconds_human_readable(secs: i64) -> String {
-    return format!("{} m {} s", secs / 60, secs % 60);
+    if secs >= 60 * 60 {
+        let hours: i64 = secs / (60 * 60);
+        let seconds_left:i64 = secs % (60 * 60);
+        return format("{} h {}", hours, render_seconds_human_readable(seconds_left));
+    }
+    else if secs >= 60 {
+        let minutes: i64 = secs / 60;
+        let seconds_left: i64 = secs % 60;
+        return format("{} m {}", render_seconds_human_readable(seconds_left));
+    }
+    else {
+        return format!("{} s", secs);
+    }
+
 }
