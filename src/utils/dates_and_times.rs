c693484d029f15b1f235c50b05ee8f1a0fd42e30
James Boland <jamespatrickboland@gmail.com>
2025-02-22 21:12:42 +0100

    Adding module for date and time helpers

---
+++ src/utils/dates_and_times.rs
@@ -0,0 +1,29 @@
+use chrono::{DateTime, Duration, Local, NaiveDate};
+use std::mem;
+
+pub struct DateRange(pub NaiveDate, pub NaiveDate);
+
+impl Iterator for DateRange {
+    type Item = NaiveDate;
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.0 <= self.1 {
+            let next = self.0 + Duration::days(1);
+            Some(mem::replace(&mut self.0, next))
+        } else {
+            None
+        }
+    }
+}
+
+
+pub fn get_local_now() -> DateTime<Local> {
+    return Local::now();
+}
+
+pub fn convert_date_to_date_str(date: NaiveDate) -> String {
+    return date.format("%Y-%m-%d").to_string();
+}
+
+pub fn get_todays_date_str() -> String {
+    return convert_date_to_date_str(get_local_now().date_naive());
+}
