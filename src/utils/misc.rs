d72693bf634d166f7a6f31d4e618b75fd76f42bd
James Boland <jamespatrickboland@gmail.com>
2025-03-02 00:13:07 +0100

    Improving how we get the user input for time

--- src/utils/misc.rs
+++ src/utils/misc.rs
@@ -1,3 +1,6 @@
+use std::collections::HashMap;
+use regex::Regex;
+
 pub fn render_seconds_human_readable(secs: i64) -> String {
     let (sign, sign_str): (i64, &str) = if secs < 0 {(-1, "-")} else {(1, "")};
     let abs_secs: i64 = sign * secs;
@@ -27,23 +30,20 @@ pub fn convert_input_to_seconds(input_str: &str) -> Result<i64, String> {
         "Malformed number of seconds. Should be either an integer or of the form: [zh][ym]xs. Got {}", input_str
     );
     let mut secs: i64 = 0;
-    let mut rest: String = input_str.to_lowercase().clone();
-    let steps: Vec<(&str, i64)> = vec![("h", 60 * 60), ("m", 60), ("s", 60)];
-    for (sep, secs_per) in steps {
-        let this_split: Vec<&str> = rest.split(sep).collect();
-        match this_split.len() {
-            0 => unreachable!("This shouldn't happen"),
-            2 => {
-                let parse_result = this_split[0].trim().parse::<i64>();
-                if let Err(_) = parse_result {
-                    return Err(err_msg);
-                }
-                secs += parse_result.expect(&err_msg) * secs_per;
-                rest = this_split[1].to_string();
-            },
-            1 => rest = this_split[0].to_string(),
-            _ => return Err(format!("Malformed number of seconds. Should be of the form: [zh][ym]xs. Got {}", input_str)),
+    let rest: String = input_str.to_lowercase().clone();
+    let units_to_secs: HashMap<&str, i64> = HashMap::from([("h", 60 * 60), ("m", 60), ("s", 1)]);
+    let re = Regex::new(r"(\d+)([hms])").unwrap();
+    
+    for (_, [amount, unit]) in re.captures_iter(&rest).map(|c| c.extract()) {
+        println!("{}, {}", amount, unit);
+        let parse_result = amount.parse::<i64>();
+        if let Err(_) = parse_result {
+            return Err(err_msg);
         }
+        let num_unit: i64 = parse_result.unwrap();
+        let multiplier_for_secs: i64 = *units_to_secs.get(unit).unwrap();
+        secs += num_unit * multiplier_for_secs;
     }
+    print!("{}", secs);
     return Ok(secs);
 }
